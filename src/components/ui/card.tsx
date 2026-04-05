d2efda2fb712a3307136c41da84edd8ec9083d19
[dyad] <confucio21@gmail.com>
2026-04-05 16:44:22 -0300

    [dyad] Corrigindo erro de sintaxe no componente Card - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -11,7 +11,7 @@ const Card = React.forwardRef<
       "rounded-lg border bg-card text-card-foreground shadow-sm",
       className
     )}
-    ...props
+    {...props}
   />
 ))
 Card.displayName = "Card"
